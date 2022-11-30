b3f919cbed025be9d4a39d69a8bcb5441c31ea14
Zuev Aleksandr <A.Zuev@stdev.su>
2022-12-01 00:19:31 +0400

    Add some unit tests

---
+++ LiteDB.Tests/Issues/Issue2253_Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+using Xunit;
+using System.Dynamic;
+
+namespace LiteDB.Tests.Issues
+{
+    public class Issue2253_Tests
+    {
+        [Fact]
+        public void DictionaryWithObject()
+        {
+            var ms = new MemoryStream();
+            var litedb = new LiteDatabase(ms);
+
+            var customProps = new Dictionary<string, object>
+            {
+                { "key1", 1 },
+                { "key2", "Hello" },
+                { "key3", true },
+                { "key4", DateTime.Now },
+                { "key5", new TimeSpan(1, 20, 0) }
+            };
+            dynamic exp = new ExpandoObject();
+            exp.prop1 = "val1";
+            exp.prop2 = "val2";
+            customProps.Add("key6", exp);
+            var scope = new UserScope
+            {
+                Policy = new Policy
+                {
+                    Properties = customProps
+                }
+            };
+
+            var collection = litedb.GetCollection<UserScope>("test");
+            collection.Insert(scope);
+
+            ms.Position = 0;
+            litedb = new LiteDatabase(ms);
+            collection = litedb.GetCollection<UserScope>("test");
+
+            var getCustomPropsBack = collection.FindOne(_ => true);
+            Assert.NotEqual(scope, getCustomPropsBack); // They are not equals because of ExpandoObject convert to object.
+        }
+
+        private class UserScope
+        {
+            public object Policy { get; set; }
+        }
+
+        private class Policy
+        {
+            public Dictionary<string, object> Properties { get; set; }
+        }
+    }
+}
---
+++ LiteDB.Tests/Issues/Issue2254_Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+using Xunit;
+
+namespace LiteDB.Tests.Issues
+{
+    public class Issue2254_Tests
+    {
+        [Fact]
+        public void SelectTest()
+        {
+            var ms = new MemoryStream();
+            var litedb = new LiteDatabase(ms);
+
+            var collection = litedb.GetCollection<TestData>();
+            collection.Insert(new TestData()
+            {
+                Guid = Guid.NewGuid(),
+                Name = "DC00001"
+            });
+
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains('0'))); // Work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains('1'))); // Work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("01"))); // Not work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("001"))); // Not work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("0001"))); // Not work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("00001"))); // Work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("C00001"))); // Work
+            Assert.NotNull(collection.FindOne(a => a.Name.Contains("DC00001"))); // Work
+        }
+
+        private class TestData
+        {
+            public Guid Guid { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
